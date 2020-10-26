f903ce685bba95a2ecfcb211c0d99bafb427c9a8
Dmitriy Morozov <rifename@gmail.com>
2020-10-26 07:01:47 -0400

    Heroku full upload commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,26 @@
+//IIFE - Immediately Invoked Function Expression
+
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...");
+
+        //confirmation delete function
+        let deleteButtons = document.querySelectorAll('.btn-danger');
+        
+        for(button of deleteButtons)
+        {
+            button.addEventListener('click', (event) => {
+                if(!confirm("Are you sure?")) 
+                {
+                    event.preventDefault();
+                    window.location.assign('/book-list');
+                }
+            });
+        }
+    }
+
+    window.addEventListener("load", Start);
+
+});
---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+//database connection file
+
+module.exports = 
+{
+    "URI": "mongodb://localhost/book_store"
+}
