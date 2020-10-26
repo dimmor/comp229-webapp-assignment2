514bf32646ac6c95b708759f7cbb5ce917697c39
Dmitriy Morozov <rifename@gmail.com>
2020-10-26 14:25:15 -0400

    online database connection commit

--- server/config/db.js
+++ server/config/db.js
@@ -2,5 +2,6 @@
 
 module.exports = 
 {
-    "URI": "mongodb://localhost/book_store"
+    //"URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://dimmor:9SqSZIW2MjzLrGYR@mongodbserver.9ag9m.mongodb.net/book_store?retryWrites=true&w=majority"
 }
